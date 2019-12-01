25c55b6fbe7d173538f4f3164877fda0f5122dca
wayne20044 <58100169+wayne20044@users.noreply.github.com>
2019-12-01 11:15:14 +0800

    Update run.sh

--- hadoop_multi_nodes/run.sh
+++ hadoop_multi_nodes/run.sh
@@ -2,8 +2,8 @@
 sudo addgroup hadoop
 sudo adduser --disabled-password  --gecos "" --ingroup hadoop hduser
 # 使用前請確認下載位置
-wget http://172.18.39.68/os/jdk-8u231-linux-x64.tar.gz
-wget http://172.18.39.68/os/hadoop-2.10.0.tar.gz
+wget http://140.135.77.85/os/2019-Autumn/jdk-8u231-linux-x64.tar.gz
+wget http://140.135.77.85/os/2019-Autumn/hadoop-2.10.0.tar.gz
 sudo tar zxvf hadoop-2.10.0.tar.gz -C /usr/local/
 sudo tar zxvf jdk-8u231-linux-x64.tar.gz -C /usr/local/
 sudo ln -s /usr/local/hadoop-2.10.0/ /usr/local/hadoop
